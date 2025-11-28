8d0a76f38b0b8371ee39c7a7cc1ac0cfb735dea0
jotjelfabri <124701555+jotjelfabri@users.noreply.github.com>
2025-11-28 07:21:54 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Toggle mobile menu
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.style.display = navLinks.style.display === "flex" ? "none" : "flex";
+});
